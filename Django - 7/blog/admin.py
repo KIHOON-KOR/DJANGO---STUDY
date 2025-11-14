03baf61972a7231990001dfad0b4307732438c73
kihoon <nike6736@naver.com>
2025-11-14 18:28:50 +0900

    first commit

---
+++ Django - 7/blog/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+
+from blog.models import Blog, Comment
+
+admin.site.register(Comment)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ['content', 'author']
+    extra = 1
+
+# @admin.register(Blog)
+# class BlogAdmin(admin.ModelAdmin):
+#     inlines = [
+#         CommentInline
+#     ]
+
+@admin.register(Blog)
+class BlogAdmin(SummernoteModelAdmin):
+    summernote_fields = ['content', ]
+    inlines = [
+        CommentInline
+    ]
---
+++ Django - 7/blog/models.py
@@ -0,0 +1,89 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from utils.models import TimestampModel
+
+from PIL import Image
+from pathlib import Path
+from io import BytesIO
+
+class Blog(models.Model):
+    CATEGORY_CHOICES = (
+        ('free', '자유'),
+        ('travel', '여행'),
+        ('cat', '고양이'),
+        ('dog', '강아지')
+    )
+
+    category = models.CharField('카테고리', max_length=10, choices=CATEGORY_CHOICES , default='free')
+    title = models.CharField('제목', max_length=100)
+    content = models.TextField('본문')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField('이미지', null=True, blank=True, upload_to='blog/%Y/%m/%d')
+
+    thumbnail = models.ImageField('썸네일', null=True, blank=True, upload_to='blog/%Y/%m/%d/thumbnail')
+
+    created_at = models.DateTimeField('작성일자', auto_now_add=True)
+    updated_at = models.DateTimeField('작성일자', auto_now=True)
+
+    # 제목이 노출되는 형식을 설정 [카테고리] 제목은 최대 10자까지
+    def __str__(self):
+        return f'[{self.get_category_display()}] {self.title[:10]}'
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'blog_pk': self.pk})
+
+    def get_thumbnail_url(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        elif self.image:
+            return self.image.url
+        return None
+
+
+    def save(self, *args, **kwargs):
+        if not self.image:
+            return super().save(*args, **kwargs)
+
+        image = Image.open(self.image)
+        image.thumbnail((300, 300))
+
+        image_path = Path(self.image.name)
+
+        thumbnail_name = image_path.stem
+        thumbnail_extension = image_path.suffix
+        thumbnail_filename = f'{thumbnail_name}_thumb{thumbnail_extension}'
+
+        if thumbnail_extension in ['.jpg', 'jpeg']:
+            file_type = 'JPEG'
+        elif thumbnail_extension == '.gif':
+            file_type = 'GIF'
+        elif thumbnail_extension == '.png':
+            file_type = 'PNG'
+        else:
+            return super().save(*args, **kwargs)
+
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, file_type)
+        temp_thumb.seek(0)
+
+        self.thumbnail.save(thumbnail_filename, temp_thumb, save=False)
+        temp_thumb.close()
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = '블로그'
+        verbose_name_plural = '블로그 목록'
+
+class Comment(TimestampModel):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    content = models.CharField('본문', max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.blog.title} 댓글'
+
+    class Meta:
+        verbose_name = '댓글'
+        verbose_name_plural = '댓글 목록'
+        ordering = ['-created_at', '-id']
